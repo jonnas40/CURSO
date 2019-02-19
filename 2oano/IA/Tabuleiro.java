781dbecddb449ade59f5b76fd02e7006594705c4
XxArcaiCxX <peuporsche@hotmail.com>
2019-02-19 17:15:19 +0000

    Create Tabuleiro.java

---
+++ 2oano/IA/Tabuleiro.java
@@ -0,0 +1,14 @@
+class Tabuleiro{
+  public static int[] Right(int[] tab) {
+    
+  }
+  public static int[] Left(int[] tab) {
+
+  }
+  public static int[] Up(int[] tab) {
+
+  }
+  public static int[] Down(int[] tab) {
+
+  }
+}
