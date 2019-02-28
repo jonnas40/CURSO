2278b8cc12a082d10dbc982255af12725ee50eef
jonnas40 <jonnas_nog@hotmail.com>
2019-02-28 17:04:19 +0000

    an

--- 2oano/AN/Projeto1/Trabalho.py
+++ 2oano/AN/Projeto1/Trabalho.py
@@ -15,12 +15,12 @@ def epsilon():
 def dalambert(cons, serie):
     print("Criterio de D'Alembert")
     f = lambda x: eval(serie)
-    y = f(1)
+    y = f(0)
     fin1 = cons * y
     fin = 0
-    for k in range(2, 500):
+    for k in range(1, 500):
         ser = f(k)
-        fin2 = cons * ser
+        fin2 = ser
         # print(k)
         fin = fin2 / fin1
         # print(fin)
@@ -34,69 +34,66 @@ def dalambert(cons, serie):
 
     acc = 0
     exp = 1.e-8
-    total = 0
+    totalf = 0
+    Rn = 1 / (1 - fin)
 
     for j in range(100):
         ser = f(j)
-        func = cons * ser
-        Rn = 1 / (1 - fin)
-        Al = func * Rn
+        totalf = totalf + ser
+        func = cons * totalf
+        Al = ser * Rn
         acc = acc + 1
-        total = total + f(j)
-        erro = math.pi - total
-        if erro < 0:
-            erro = -erro
+        erro = math.fabs(math.pi - func)
 
         if Al < exp:
-            n = j
             print("O n-ésimo termo com erro absoluto inferior a ", "{:.0E}".format(exp), " é: ", j,
-                  ", com o valor aproximado de: ", total,". E o erro absoluto: ",erro)
+                  ", com o valor aproximado de: ", func,". E o erro absoluto: ",erro)
             exp = exp / 10
             if exp < 1.e-15:
                 break
+
 
 
 def cauchy(cons, serie):
     print("Critério de Cauchy")
     f = lambda x: eval(serie)
     ter = 0
-    for k in range(1, 500):
-        ser = f(k)
-        ter1 = cons * ser
-        #k1 = 1/float(k)
-        ter = ter1**(1/float(k))
-        #ter = math.pow(ter1, k1)
-        ter = ter.real
-
-        if ter > 1:
-            print("A serie diverge")
-        elif ter == 1:
-            print("Nada se pode concluir")
-            return
-
     acc = 0
     exp = 1.e-8
-    total = 0
+    totalf = 0
 
-    for j in range(100):
-        Rn = math.pow(ter, j - 1) / 1 - ter
-        acc = acc + 1
-        total = total + f(j)
-        erro = math.fabs(math.pi - total)
-
-
-        if Rn < exp:
-            n = j
-            print("O n-ésimo termo com erro absoluto inferior a ", "{:.0E}".format(exp), " é: ", j, ", com o valor aproximado de: ", total,". E o erro absoluto: ",erro)
+    for k in range(0, 10000):
+        ser = f(k)
+        totalf = totalf + ser
+        #print(math.fabs(ser))
+        if math.fabs(ser) <= exp:
+            total = cons * totalf
+            erro = math.fabs(math.pi - total)
+            print("O n-ésimo termo com erro absoluto inferior a ", "{:.0E}".format(exp), " é: ", k,
+                  ", com o valor aproximado de: ", total, ". E o erro absoluto: ", erro)
             exp = exp / 10
             if exp < 1.e-15:
                 break
 
 
+""""
+    for j in range(100):
+        Rn = math.pow(ter, j - 1) / 1 - ter
+        acc = acc + 1
+        ser = f(j)
+        totalf = totalf + ser
+        total = cons * totalf
+        erro = math.fabs(math.pi - total)
+
+
+        if Rn < exp:
+            """
+
+
 cons = (9 / (2 * math.sqrt(3)))
-serie = "(math.factorial(x)**2) / (math.factorial(2*x+1))"
+serie = "((math.factorial(x))**2) / (math.factorial((2*x)+1))"
 dalambert(cons, serie)
 print()
 cons1 = 4
-serie1 = "(((-1)**(x))/(2*(x)+1))"
+serie1 = "(((-1)**(x))/((2*(x))+1))"
 cauchy(cons1, serie1)
