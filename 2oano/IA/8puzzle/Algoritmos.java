ff8a2070b64cf42c73409a9ea0a787ff95400f47
jonnas40 <jonnas_nog@hotmail.com>
2019-02-28 14:41:29 +0000

    l

--- 2oano/IA/8puzzle/Algoritmos.java
+++ 2oano/IA/8puzzle/Algoritmos.java
@@ -1,10 +1,7 @@
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
-import java.util.TreeMap;
 import java.util.PriorityQueue;
 
 class Algoritmos{
