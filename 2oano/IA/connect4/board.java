217dce6353023e84882bbd014119502b9ac112ab
jonnas40 <jonnas_nog@hotmail.com>
2019-03-12 18:01:56 +0000

    yeet

--- 2oano/IA/connect4/board.java
+++ 2oano/IA/connect4/board.java
@@ -1,26 +1,45 @@
 import java.util.Arrays;
+import java.util.HashSet;
 
 class Board {
 
     char[][] board;
+    char turn;
+    char nextTurn;
 
-    public newBoard(){
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; i < 6; i++) {
+    public Board(){
+        board = new char[6][7];
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
                 this.board[i][j] = '-';
             }
         }
     }
 
-    public Board sons(Board pai){
-        
+    public static void copyBoard(Board src, Board target){
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 7; j++) {
+                target.board[i][j] = src.board[i][j];
+            }
+        }
+        target.nextTurn = src.turn;
+        target.turn = src.nextTurn;
     }
 
-    public void play(int n, char c){
+    public Set sons(Board pai){
+        Set<Board> sons = new HashSet<Board>();
+        for (int i = 0; i < 7; i++) {
+            sons.add(play(i, pai.nextTurn));
+        }
+    }
+
+    public Board play(int n, char c){
         int i = 0;
-        while (this.board[n][i] == '-') {
+        Board ret = new Board();
+        copyBoard(this, ret);
+        while (ret.board[i][n] == '-') {
             i++;
         }
-        this.board[n][i-1] = c;
+        return ret.board[i-1][n] = c;
     }
 }
