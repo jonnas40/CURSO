46c4eccfe66d0575c20e5d5234977cabe6c261f9
jonnas40 <jonnas_nog@hotmail.com>
2019-03-12 18:14:21 +0000

    printboard no board

--- 2oano/IA/connect4/board.java
+++ 2oano/IA/connect4/board.java
@@ -15,6 +15,15 @@ class Board {
         }
     }
 
+    public static void printBoard(Board tab){
+        System.out.println("+-------------+");
+        for (int i=5; i>=0; i--){
+          System.out.print("|" + tab.board[i][0] + "|" + tab.board[i][1] + "|" + tab.board[i][2] + "|" + tab.board[i][3] + "|" + tab.board[i][4] + "|" + tab.board[i][5] + "|" + tab.board[i][6] + "|\n");
+        }
+        System.out.println("+-------------+");
+        System.out.println("|0|1|2|3|4|5|6|");
+    }
+
     public static void copyBoard(Board src, Board target){
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 7; j++) {
