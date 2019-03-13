e13e870c626560a9021953e5be48afd652ea8fa2
XxArcaiCxX <peuporsche@hotmail.com>
2019-03-13 21:28:43 +0000

    Corrigido colunas não existentes

--- 2oano/IA/connect4/Connect4.java
+++ 2oano/IA/connect4/Connect4.java
@@ -2,17 +2,23 @@ import java.util.Scanner;
 
 class Connect4{
 
-
   public static void main(String[] args) {
     Scanner stdin = new Scanner(System.in);
     Board tab = new Board();
     Boolean win = false;
     while(win!=true){
         Board.printBoard(tab);
-        tab = tab.play(stdin.nextInt());
-        win = tab.checkWin();
+        int i = stdin.nextInt();
+        if (i<7 && i>=0){
+          tab = tab.play(i);
+          win = tab.checkWin();
+        }
+        else {
+          System.out.println("Coluna não existente");
+        }
     }
     Board.printBoard(tab);
     System.out.println(tab.turn + " wins");
   }
+
 }
