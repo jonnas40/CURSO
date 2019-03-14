9aa348290e6ce92148f4036131b3826a004ad271
jonnas40 <jonnas_nog@hotmail.com>
2019-03-14 18:09:50 +0000

    so

--- 2oano/SO/Ficha 4/mytouch.c
+++ 2oano/SO/Ficha 4/mytouch.c
@@ -2,22 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/uio.h>
 
 int main(int argc, char* argv[]) {
 
-    struct stat info;
-
-    if (argc < 1) {
+    if (argc != 2) {
         fprintf(stderr, "usage: %s file\n", argv[0]);
         return EXIT_FAILURE;
     }
         
-    if (lstat(argv[1], &info) == -1) {
-        fprintf(stderr, "fsize: Can’t stat %s\n", argv[1]);
-        return EXIT_FAILURE;
+    FILE *fp;
+    fp = fopen(argv[1], "w");
+    if(fp!=NULL){
+
     }
-        
-    printf("%s size: %d bytes, disk_blocks: %d, owner: %d, last changed: %s\n", argv[i], (int)info.st_size, (int)info.st_blocks, info.st_uid, ctime(&info.st_mtime));  
+    else{
+        open(argv[1], O_RDWR|O_CREAT, 0644);
+        printf("File created\n");
+    } 
 
     return EXIT_SUCCESS;
 
