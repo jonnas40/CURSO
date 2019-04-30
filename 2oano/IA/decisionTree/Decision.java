a39f7887460e9643553a9d68567d1088e35335a1
XxArcaiCxX <peuporsche@hotmail.com>
2019-04-30 16:32:48 +0100

    Efetuado o catch do erro

--- 2oano/IA/decisionTree/Decision.java
+++ 2oano/IA/decisionTree/Decision.java
@@ -3,10 +3,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 class Decision{
-    
+
     static Atribute classe;
     public static void main(String[] args) {
-        
+
         //leitura e parsing dos csv's
         String fileName = args[0];
         File file = new File(fileName);
@@ -50,19 +50,19 @@ class Decision{
             e.printStackTrace();
         }*/
 
-        
-        
-        
+
+
+
         /*for (String[] var : exa) {
             for (String ex : var) {
                 System.out.print(ex + " ");
             }
             System.out.println();
         }*/
-        
+
         //classe.printAtr();
-        
-        
+
+
         List<String[]> examples = new ArrayList<String[]>();
         Node tree = ID3(values, atributos, examples);
         DFS(tree, "");
@@ -75,11 +75,15 @@ class Decision{
             exa.add(buf);
             in = sc.next();
         }
-        exa = discrete(exa);
-        int i = 1;
-        for (String[] ex : exa) {
+        try {
+          exa = discrete(exa);
+          int i = 1;
+          for (String[] ex : exa) {
             System.out.println("Exemplo " + i + ":" + classify(tree, ex, atributos2));
             i++;
+          }
+        }catch(IndexOutOfBoundsException e) {
+          return;
         }
     }
 
@@ -135,7 +139,7 @@ class Decision{
     }
 
     //função entropia e importancia
-    
+
     public static double entropy(Atribute a, List<String[]> values){
         //double total = values.size();
         //double gain=0;
@@ -150,9 +154,9 @@ class Decision{
         }
         return entropy;
     }
-    
+
     //função probabilidade
-    
+
     public static double prob(String aux, String var, int index, List<String[]> values){
         int counter = 0;
         int total = 0;
@@ -165,9 +169,9 @@ class Decision{
         if(total==0 || counter==0) return 0;
         else return (double)total/values.size()*(-((double)counter/total * log2((double)counter/total)));
     }
-    
+
     //função plurality value
-    
+
     public static Node pluralityValue(List<String[]> values){
         int[] counter = new int[classe.noVal()];
         for (String[] var : values) {
@@ -194,7 +198,7 @@ class Decision{
     }
 
     //função auxiliar
-    
+
     public static Boolean allSame(List<String[]> values){
         String[] aux = values.get(0);
         /*for (String a : aux) {
@@ -209,17 +213,17 @@ class Decision{
     }
 
     ////////auxiliares matemáticas
-    
+
     public static double logb( double a, double b ){
         return Math.log(a) / Math.log(b);
     }
-    
+
     public static double log2( double a ){
         return logb(a,2);
     }
-    
+
     //DFS
-    
+
     public static void DFS(Node n, String space){
         System.out.println(space+n);
         if (n.isLeaf()) return;
@@ -227,13 +231,13 @@ class Decision{
             DFS(son, space + "\t");
         }
     }
-    
+
     //classificação de exemplos dados
 
     public static String classify(Node tree, String[] example, LinkedList<Atribute> atributos){
         if (tree.isLeaf())
             return tree.getData();
-        
+
         String atr = tree.getData();
         int index = 0;
         //System.out.println(atr);
@@ -249,11 +253,11 @@ class Decision{
             if (example[index].equals(son.getParent())){
                 //System.out.println(son.getData());
                 return classify(son, example, atributos);
-            } 
+            }
         }
         return "Not found";
     }
-        
+
 
     //torna os dados continuos em discretos
 
